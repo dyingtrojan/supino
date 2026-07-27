621d45a2f6e78e0873aebd465151f70e2eab2bb0
eswo <eswooow@gmail.com>
2026-07-27 00:48:52 -0300

    fix: old variable names for tool calling

--- main.py
+++ main.py
@@ -1,87 +1,69 @@
 import ollama, subprocess, pyfiglet
-from tools import apps_handler, toast_notification, code_runner, website_opener
-from config import setup, preload
+from tools import apps_handler, toast_notification, code_runner, website_handler
+from config import settings, setup
 from pathlib import Path
+
+first_messsage = ""
 
 subprocess.run(['cls'], shell=True)
 print(pyfiglet.figlet_format("mace's", font='digital'))
 print(pyfiglet.figlet_format("S U P I N O", font='nipples'))
-preload.load_settings()
+settings.load_settings()
 
-def open_website(url=""):
-    return website_opener.open_website(url)
-
-def send_toast(title, body, app_name):
-    return toast_notification.send_toast(title, body, model_name)
-    
-def run_commands(code=r''):
-    return code_runner.run_command(code)
-
-def find_app_path(app_name=r''):
-    return apps_handler.find_app(app_name)
-
-def start_app(path=r''):
-    return apps_handler.start_app(path)
-
-
-
-tools = [send_toast, run_commands, start_app, find_app_path, open_website]
+tools = [toast_notification.send_toast, code_runner.run_command, apps_handler.start_app, apps_handler.find_app, website_handler.open_website]
 messages = []
-system_prompt = preload.settings['system_prompt']
-model_name = preload.settings['model']
+system_prompt = settings.settings['system_prompt']
+model_name = settings.settings['model']
 
-if preload.history_path.is_file():
-    use_history = input("Há um historico de conversa já existente. Quer usar-lo agora? \n (y/n)")
-    if use_history.lower() == 'y':
-        messages = preload.load_history()
-        messages.append({"role": "user", "content": "Eu voltei."})
-    if use_history.lower() == 'n':
+if settings.history_path.is_file():
+    use_history = input("There is an chat history saved. Do you want to proceed this conversation?\n (y/n)")
+    if use_history.lower() == 'y' or use_history.lower() == 'yes':
+        messages = settings.load_history()
+        messages.append({"role": "user", "content": "I'm Back."})
+        
+    if use_history.lower() == 'n' or use_history.lower() == 'no':
         messages = [
         {"role": "system", "content": system_prompt},
-        {"role": "user", "content": "Olá!"},
+        {"role": "user", "content": "Hi!"},
         ]
-
-        anwser = ollama.chat(model=model_name, messages=messages, tools=tools, stream=True)
-        content = ""
-        print("Bot: ", end='', flush=True)
-        for chunk in anwser:
-            print(chunk.message.content, end='', flush=True)
-            content += chunk.message.content
-        print("\n")
-        messages.append({"role": "assistant", "content": content})
-        preload.add_to_history({"role": "assistant", "content": content})
 else:
-    messages = [
-        {"role": "system", "content": system_prompt},
-        {"role": "user", "content": "Hello!"},
-    ]
+    try:
+        while not first_messsage:
+            first_messsage = input("Type a little text about you, introduce yourself to your AI agent: ")
+            settings.add_to_history({"role": "user", "content": first_messsage})
+            messages = [
+                {"role": "system", "content": system_prompt},
+                {"role": "user", "content": first_messsage}
+            ]
+    except Exception as e:
+        print(f"An error occured: {e}")
 
-    anwser = ollama.chat(model=model_name, messages=messages, tools=tools, stream=True)
-    content = ""
-    print("Bot: ", end='', flush=True)
-    for chunk in anwser:
-        print(chunk.message.content, end='', flush=True)
-        content += chunk.message.content
-    print("\n")
-    messages.append({"role": "assistant", "content": content})
-    preload.add_to_history({"role": "assistant", "content": content})
+anwser = ollama.chat(model=model_name, messages=messages, tools=tools, stream=True)
+content = ""
+print("Bot: ", end='', flush=True)
+for chunk in anwser:
+    print(chunk.message.content, end='', flush=True)
+    content += chunk.message.content
+print("\n")
+messages.append({"role": "assistant", "content": content})
+settings.add_to_history({"role": "assistant", "content": content})
 
 while True:
     content = ""
     question = input("User: ")
-    
     if question.lower() == "quit":
-        preload.add_to_history({"role": "user", "content": "Tchau"})
-        preload.save_history()
+        settings.add_to_history({"role": "user", "content": "Bye!"})
+        settings.save_history()
         break
     
     messages.append({"role": "user", "content": question})
-    preload.add_to_history({"role": "user", "content": question})
+    settings.add_to_history({"role": "user", "content": question})
     
     anwser = ollama.chat(model=model_name, messages=messages, tools=tools, stream=True)
     tool_calls = []
-    available_functions = {"send_toast": send_toast, "run_commands": run_commands, "start_app": start_app, "find_app_path": find_app_path, "open_website": open_website}
+    available_functions = {"send_toast": toast_notification.send_toast, "run_commands": code_runner.run_command, "start_app": apps_handler.start_app, "find_app": apps_handler.find_app, "open_website": website_handler.open_website}
     tools_index = 0
+    print('\n')
     print("Bot: ", end='', flush=True)
     for chunk in anwser:
         print(chunk.message.content, end='', flush=True)
@@ -95,7 +77,7 @@ while True:
             if func:
                 result = func(**tool_called.function.arguments)
                 messages.append({"role": "tool", "name": tool_called.function.name, "content": str(result)})
-                preload.add_to_history({"role": "tool", "name": tool_called.function.name, "content": str(result)})
+                settings.add_to_history({"role": "tool", "name": tool_called.function.name, "content": str(result)})
                 tools_index += 1
     print("\n")
     if tool_calls:
@@ -107,7 +89,7 @@ while True:
             response += chunk.message.content
         print("\n")
         messages.append({"role": "assistant", "content": response})
-        preload.add_to_history({"role": "assistant", "content": response})
+        settings.add_to_history({"role": "assistant", "content": response})
     assistant_reply = content
     tools_index = 0
     tool_calls = []
