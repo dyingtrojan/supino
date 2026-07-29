d650d911a0c9f5d2995f3e2ed07c0929b5bab790
eswo <eswooow@gmail.com>
2026-07-29 09:41:14 -0300

    added async tts

--- speech/text_to_speech.py
+++ speech/text_to_speech.py
@@ -1,7 +1,15 @@
 import pyttsx3 as ttspy
+import threading
 
-def speak(text = ""):
-    tts = ttspy.init()
-    print("Speaking...")
+tts = ttspy.init()
+
+def _speak_sync(text=""):
     tts.say(text)
     tts.runAndWait()
+
+def speak(text = ""):
+    if not text:
+        return
+    
+    thread = threading.Thread(target=_speak_sync, args=(text, ), daemon=True)
+    thread.start()
